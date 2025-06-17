7fc93aa9483678c9faa6199ce4c690aa5e3f8c13
yoonbeeseo <yoon.tec.info@gmail.com>
2025-06-17 11:45:20 +0900

    pop-corn

--- src/components/BeforeLogin/BeforeLayout.tsx
+++ src/components/BeforeLogin/BeforeLayout.tsx
@@ -1,9 +1,17 @@
 import HeroSection from "./HeroSection";
+import Popcorn from "./Popcorn";
 
 const BeforeLayout = () => {
   return (
     <main>
       <HeroSection />
+      <div className="con">
+        <Popcorn />
+        <p>asdfasd</p>
+        <p>asdfasd</p>
+        <p>asdfasd</p>
+        <p>asdfasd</p>
+      </div>
     </main>
   );
 };
