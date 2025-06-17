91502b8da8ac8bf285b7dc3ac82eb6ecaa40ecbf
yoonbeeseo <yoon.tec.info@gmail.com>
2025-06-17 10:46:23 +0900

    hero-section

---
+++ src/components/BeforeLogin/BeforeLayout.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "./HeroSection";
+
+const BeforeLayout = () => {
+  return (
+    <main>
+      beforelayout
+      <HeroSection />
+    </main>
+  );
+};
+
+export default BeforeLayout;
